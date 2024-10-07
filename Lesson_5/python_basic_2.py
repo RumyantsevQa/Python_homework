85f7595da246ede37b9ff05e9e8b98e706fa775e
iliya <rumyantsev44@yandex.ru>
2024-10-07 21:31:20 +0300

    modified python_basic_2.py

--- Lesson_5/python_basic_2.py
+++ Lesson_5/python_basic_2.py
@@ -13,13 +13,39 @@
 # Срез
 # Извлечение среза позволяет взять из списка определенную его часть.
 
-lll = [1, 3, 5, 7, 8, 11, 9]
-print(lll)
-print(lll[1:4])
-print(lll[:2])
-print(lll[3:])
-print(lll[1::2])
-print(lll[:])
-print(lll[::-1])
-print(lll[::-2])
-print(lll[-2:-6:-1])
+# lll = [1, 3, 5, 7, 8, 11, 9]
+# print(lll)
+# print(lll[1:4])
+# print(lll[:2])
+# print(lll[3:])
+# print(lll[1::2])
+# print(lll[:])
+# print(lll[::-1])
+# print(lll[::-2])
+# print(lll[-2:-6:-1])
+
+
+# Методы строк
+# Со строкой можно делать многое из того, что можно делать с другими коллекциями, т.к. строка это по сути тоже коллекция - последовательность символов.
+# Больше всего функциональность строки похожа на функциональность кортежей.
+
+# text = 'my long long string'
+# print(text[3])
+# print(len(text))
+# print(text.index('long'))
+# print('long' in text)
+# print(text.count('long'))
+# print(text.find('long'))
+# print(text[3:-6])
+# print(text.startswith('my'))
+# print(text.endswith('string'))
+
+# txt = "ThIs tExt wiTh meSsEd uP CaPITalIZatiOn!"
+# print(txt.capitalize()) #Делает первую букву предложения заглавной
+# print(txt.title()) #Делает каждую первую букву заглавной
+# print(txt.upper()) #Делает все буквы большими
+# print(txt.lower()) #Делает все буквы маленькими
+
+text = "ThIs tExt wiTh meSsEd uP CaPITalIZatiOn!"
+string_index = text.index('CaPITalIZatiOn')
+print(text[:string_index].lower() + text[string_index:].upper())
